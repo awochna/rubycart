5b1585a728467e33cd1ce734f706c7aff68b282b
Alex Wochna <awochna@email.arizona.edu>
2013-04-26 09:54:33 -0700

    Initial commit; initialize rails application

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Rubycart::Application.config.secret_key_base = 'b35527e83cf577cf6a2c8e5ac7175f6f2d94952f1487c10f13bddc90bba0c215695551c6adcddb14f80326908fe86e88ff7243c79a94b8839c2bb89bfdc79265'
